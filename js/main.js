d7c514ea6b5c2ad068be2c1f34ff4b6c99c14517
roze <110625211+wonder0128@users.noreply.github.com>
2023-12-29 18:59:56 +0900

    20231229

--- js/main.js
+++ js/main.js
@@ -84,3 +84,27 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+    gsap.to(
+        selector, // 선택자
+        random(1.5, 2.5), // 애니메이션 동작 시간
+        { // 옵션
+            y: size,
+            repeat: -1, // 무한 반복
+            yoyo: true, // 애니메이션 1번 진행 후 다시 돌아올 수 있게 함.
+            ease: Power1.easeInOut,
+            delay: random(0, delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
